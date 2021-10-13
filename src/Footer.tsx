da9189b16d0ae16f65226da2dc52c75ef940ab01
samzimmerman95 <samzimmerman95@gmail.com>
2021-10-12 19:43:45 -0700

    Resolve link warnings

--- src/Footer.tsx
+++ src/Footer.tsx
@@ -8,14 +8,25 @@ export default function Footer() {
           className="link-icon"
           href="https://twitter.com/Potato_Club_"
           target="_blank"
+          rel="noreferrer"
         >
           <i className="fab fa-twitter fa-2x px-3"></i>
         </a>
 
-        <a className="link-icon" href="" target="_blank">
+        <a
+          className="link-icon"
+          href="https://opensea.io"
+          target="_blank"
+          rel="noreferrer"
+        >
           <i className="fab fa-discord fa-2x px-3"></i>
         </a>
-        <a className="link-icon" href="https://opensea.io" target="_blank">
+        <a
+          className="link-icon"
+          href="https://opensea.io"
+          target="_blank"
+          rel="noreferrer"
+        >
           <img
             src={require("./assets/OpenSea.svg").default}
             className="edit-link-icon link-photo mx-3 mb-3 mb-md-0"
@@ -26,6 +37,7 @@ export default function Footer() {
           className="link-icon"
           href="https://rinkeby.etherscan.io/address/0xE7C18a3E60FC387f3Ae2725BF92811AAD05865f3"
           target="_blank"
+          rel="noreferrer"
         >
           <i className="fas fa-file-contract fa-2x px-3"></i>
         </a>
